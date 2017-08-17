fb437405a3ff26d43bd894396afdd7be236e0adc
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-08-17 14:46:14 +0200

    Add nested lists to reading map from file

--- Main.py
+++ Main.py
@@ -1,19 +1,46 @@
 import os
+import sys
+import tty
+import termios
 
 
 def read_map_from_file(filename):
+
+    board = []
+
     with open(filename) as f:
-        read_data = f.read()
-    return read_data
+        read_data = f.read().splitlines()
+
+    board = [list(x) for x in read_data]
+
+    return board
 
 
-def print_map(map):
-    print(map)
+def print_map(board):
+
+    for row in board:
+        print(''.join(row))
+        # print(row)
+
+
+def getch():
+
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
     print_map(read_map_from_file('Map1.txt'))
+
+    x = getch()
+    print(x)
 
 
 if __name__ == '__main__':
