2fc947ebd0f8e5fb07fa60956695d216ea562138
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-08-30 17:54:45 +0200

    Player can explode wall which blocked pass to next lvl

--- tnt.py
+++ tnt.py
@@ -3,46 +3,43 @@ import print_board
 
 
 def ask_explode_intent(y, x, board, player_stats):
-    yes = ['YES', 'yes', 'Yes']
     while True:
-        answer = input('Do you want to blow up this wall? ')
-        if answer in yes:
+        answer = input('Do you want to blow up this wall? ').lower()
+        if answer == 'yes':
             explode(y, x, board, player_stats)
             break
-        if answer in no:
+        else:
             break
 
 
 def explode(y, x, board, player_stats):
-    time.sleep(2)
-    board[y][x+1] = '*'
+    board[y][x+6] = '*'
     print_board.print_map(y, x, board, player_stats, '@')
-    time.sleep(0.4)
 
-    board[y-1][x+1] = '*'
-    board[y+1][x+1] = '*'
-    board[y][x+2] = '*'
-    board[y][x] = '*'
-    print_board.print_map(y, x, board, player_stats, '@')
-    time.sleep(0.4)
+    blow_y = 1
+    while blow_y != 3:
+        for i in range(7, 14):
+            board[y][x+i] = '*'
+            board[y+blow_y][x+i] = '*'
+            board[y-blow_y][x+i] = '*'
+            board[y][(x+14)-i] = '*'
+            board[y+blow_y][(x+14)-i] = '*'
+            board[y-blow_y][(x+14)-i] = '*'
+            print_board.print_map(y, x, board, player_stats, '@')
+            time.sleep(0.05)
+        blow_y += 1
 
-    board[y+1][x+2] = '*'
-    board[y-1][x] = '*'
-    board[y-1][x+2] = '*'
-    board[y+1][x] = '*'
-    print_board.print_map(y, x, board, player_stats, '@')
-    time.sleep(0.4)
-    clear_explosion(y, x, board)
+    clear_y = 1
+    while clear_y != 3:
+        for i in range(7, 14):
+            board[y][x+i] = ' '
+            board[y+clear_y][x+i] = ' '
+            board[y-clear_y][x+i] = ' '
+            board[y][(x+14)-i] = ' '
+            board[y+clear_y][(x+14)-i] = ' '
+            board[y-clear_y][(x+14)-i] = ' '
+            print_board.print_map(y, x, board, player_stats, '@')
+            time.sleep(0.05)
+        clear_y += 1
+
     return board
-
-
-def clear_explosion(y, x, board):
-    board[y][x+1] = ' '
-    board[y-1][x+1] = ' '
-    board[y+1][x+1] = ' '
-    board[y][x+2] = ' '
-    board[y][x] = ' '
-    board[y+1][x+2] = ' '
-    board[y-1][x] = ' '
-    board[y-1][x+2] = ' '
-    board[y+1][x] = ' '
